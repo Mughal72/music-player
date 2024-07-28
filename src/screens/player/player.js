dbf9f9cfd67e876085ad83890f4db65b90e0402f
Anber <ambersattarmh@gmail.com>
2024-07-28 16:42:03 +0500

    initial commit

--- src/screens/player/player.js
+++ src/screens/player/player.js
@@ -4,30 +4,42 @@ import { useLocation } from "react-router-dom";
 import apiClient from "../../spotify";
 import SongCard from "../../components/songCard";
 import Queue from "../../components/queue";
+import AudioPlayer from "../../components/audioPlayer/audioPlayer";
 
 export default function Player() {
   const location = useLocation();
-  const [track, setTrack] = React.useState([]);
+  const [tracks, setTracks] = useState([]);
   const [currentTrack, setCurrentTrack] = useState({});
-  const [curentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     if (location.state) {
       apiClient
         .get("playlists/" + location.state?.id + "/tracks")
         .then((res) => {
-          setTrack(res.data.items);
-          setCurrentTrack(res.data.items[0].track);
+          setTracks(res.data.items);
+          setCurrentTrack(res.data?.items[0]?.track);
         });
     }
   }, [location.state]);
 
+  useEffect(() => {
+    setCurrentTrack(tracks[currentIndex]?.track);
+  }, [currentIndex, tracks]);
+
   return (
     <div className="screen-container flex">
-      <div className="left-player-body"></div>
+      <div className="left-player-body">
+      <AudioPlayer
+          currentTrack={currentTrack}
+          total={tracks}
+          currentIndex={currentIndex}
+          setCurrentIndex={setCurrentIndex}
+        />
+      </div>
       <div className="right-player-body">
-        <SongCard album={currentTrack.album} />
-        <Queue />
+        <SongCard album={currentTrack?.album} />
+        <Queue tracks={tracks} setCurrentIndex={setCurrentIndex} />
       </div>
     </div>
   );
